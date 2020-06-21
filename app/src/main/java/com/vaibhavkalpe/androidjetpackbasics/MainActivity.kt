56d3e11effd879a5494e9eb6d1f52781f4a4e424
Vaibhav <vaibhav@email.com>
2020-06-21 16:42:22 +0530

    Integrate mainViewModel and display addition

--- app/src/main/java/com/vaibhavkalpe/androidjetpackbasics/MainActivity.kt
+++ app/src/main/java/com/vaibhavkalpe/androidjetpackbasics/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +17,29 @@ class MainActivity : AppCompatActivity() {
         val finalAnswer = findViewById<TextView>(R.id.final_answer)
         val addNumbers = findViewById<Button>(R.id.add_numbers)
 
+        val mainModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
         addNumbers.setOnClickListener {
-            //TODO: perform addition here and display result
+            /*
+            We should not save any data in activity or fragment. We can but we should not for now.
+            Make a practice to save data in viewModel. Read the ViewModel docs again after
+            seeing this example
+            */
+            mainModel.firstNumber = firstNumber.text.toString().toInt()
+            mainModel.secondNumber = secondNumber.text.toString().toInt()
+
+            //Finally, this is how we show the  result.
+            finalAnswer.text = mainModel.getAddition().toString()
+
+            /*
+            This is how viewModel should be used. That's the basic concept of separation of concern.
+            All the data is saved into viewModel and calculation too happens in viewModel.
+            Our activity should be free from business logic. Activity should be only concerned with
+            displaying the data or presenting the data to user.
+
+            Do not confuse yourselves that you have to use viewModel with other components like
+            LiveData, View Binding and Data Binding. All these components can be used standalone.
+            */
         }
 
     }
