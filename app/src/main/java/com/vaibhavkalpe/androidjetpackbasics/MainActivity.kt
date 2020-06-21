c5259a313105b468b65f3cf4f2966ab7aff12b26
Vaibhav <vaibhav@email.com>
2020-06-21 15:58:21 +0530

    Initial commit

---
+++ app/src/main/java/com/vaibhavkalpe/androidjetpackbasics/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vaibhavkalpe.androidjetpackbasics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
