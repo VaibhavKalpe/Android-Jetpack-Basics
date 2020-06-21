2761bc1bf6bc7e06a13f4b5019a3429db07c3a37
Vaibhav <vaibhav@email.com>
2020-06-21 17:06:23 +0530

    Set mainactivity layout using view binding and removed all findViewById

--- app/src/main/java/com/vaibhavkalpe/androidjetpackbasics/MainActivity.kt
+++ app/src/main/java/com/vaibhavkalpe/androidjetpackbasics/MainActivity.kt
@@ -6,25 +6,32 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import com.vaibhavkalpe.androidjetpackbasics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val firstNumber = findViewById<EditText>(R.id.first_number)
-        val secondNumber = findViewById<EditText>(R.id.second_number)
-        val finalAnswer = findViewById<TextView>(R.id.final_answer)
-        val addNumbers = findViewById<Button>(R.id.add_numbers)
+        //Here we have set the layout of activity using view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val rootView = binding.root
+        setContentView(rootView)
+
+        //There is no need to use findViewById now. Just use binding object to access
+        //all view elements declared in layout.
+        //Note: You need to declare the id of view to access it via binding object
 
         val mainModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        addNumbers.setOnClickListener {
+        binding.addNumbers.setOnClickListener {
 
-            mainModel.firstNumber = firstNumber.text.toString().toInt()
-            mainModel.secondNumber = secondNumber.text.toString().toInt()
+            mainModel.firstNumber = binding.firstNumber.text.toString().toInt()
+            mainModel.secondNumber = binding.secondNumber.text.toString().toInt()
 
-            finalAnswer.text = mainModel.getAddition().toString()
+            binding.finalAnswer.text = mainModel.getAddition().toString()
         }
 
     }
