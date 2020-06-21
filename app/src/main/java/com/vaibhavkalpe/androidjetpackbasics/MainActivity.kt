9cfe41176fd87993a970b7e439d1ac6f80f4e637
Vaibhav <vaibhav@email.com>
2020-06-21 16:19:32 +0530

    Intialise all the views

--- app/src/main/java/com/vaibhavkalpe/androidjetpackbasics/MainActivity.kt
+++ app/src/main/java/com/vaibhavkalpe/androidjetpackbasics/MainActivity.kt
@@ -2,10 +2,23 @@ package com.vaibhavkalpe.androidjetpackbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstNumber = findViewById<EditText>(R.id.first_number)
+        val secondNumber = findViewById<EditText>(R.id.second_number)
+        val finalAnswer = findViewById<TextView>(R.id.final_answer)
+        val addNumbers = findViewById<Button>(R.id.add_numbers)
+
+        addNumbers.setOnClickListener {
+            //TODO: perform addition here and display result
+        }
+
     }
 }
