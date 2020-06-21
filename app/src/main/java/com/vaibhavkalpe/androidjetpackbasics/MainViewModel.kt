5a1321d0973c70fe69e780b2bb4a1d5fd3b4de25
Vaibhav <vaibhav@email.com>
2020-06-21 17:33:30 +0530

    Use LiveData in ViewModel

--- app/src/main/java/com/vaibhavkalpe/androidjetpackbasics/MainViewModel.kt
+++ app/src/main/java/com/vaibhavkalpe/androidjetpackbasics/MainViewModel.kt
@@ -1,13 +1,39 @@
 package com.vaibhavkalpe.androidjetpackbasics
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
     var firstNumber = 0
     var secondNumber = 0
 
-    fun getAddition() : Int {
-        return firstNumber + secondNumber
+    /**
+     * 2.Read this after 1
+     *
+     * _finalResult: is mutable live data. It can be changed by anyone who can access it.
+     * so that's why to allow access to it, we use a wrapper, liveData named finalResult, because
+     * LiveData is immutable and MutableLiveData is mutable. This way nobody can change value of
+     * final answer from outside and only read it.
+     * This concept is also known as Encapsulation or hiding the details of implementations
+     *
+     * finalResult: is linked with _finalResult. Whenever _finalResult changes changes finalResult
+     * also changes.
+     */
+    private val _finaResult = MutableLiveData<Int>()
+    val finalResult: LiveData<Int> = _finaResult
+
+    fun performAddition() {
+        val answer = firstNumber + secondNumber
+
+        /**
+         * 1.Read this first
+         *
+         * Here we saved the final answer in the _finalResult object's value property
+         * Note that our function is not returning this to anyone. We are simply updating the value
+         * of _finalResult whenever this function is called.
+         * */
+        _finaResult.value = answer
     }
 }
